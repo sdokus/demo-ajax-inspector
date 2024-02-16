6227de61e316084ce74544f586434991f5b2507b
Sami Dokus <sami2464@gmail.com>
2024-02-16 11:19:00 -0700

    commenting out attempt for switching to tribe_assets() - I will come back to this

--- src/Ajax_Inspector/Plugin.php
+++ src/Ajax_Inspector/Plugin.php
@@ -19,6 +19,11 @@ if ( ! class_exists( 'Dokus__Ajax__Inspector' ) ) {
 	 */
 	class Dokus__Ajax__Inspector {
 
+//		public $plugin_file;
+//      public $plugin_dir;
+//		public $plugin_path;
+//		public $plugin_url;
+
 		/**
 		 * Static Singleton Holder
 		 *
@@ -45,16 +50,22 @@ if ( ! class_exists( 'Dokus__Ajax__Inspector' ) ) {
 		protected function __construct() {
 			$this->enable_hooks();
 
-            do_action( 'ajax_plugin_loaded');
+//  Come back to this - should allow the plugin to find the assets:
+//			$this->plugin_file = trailingslashit( dirname( __FILE__ ) ) . '../../demo-ajax-inspector.php';
+//			$this->plugin_path = trailingslashit( dirname( $this->plugin_file ) );
+//			$this->plugin_dir = trailingslashit( basename( $this->plugin_path ) );
+//			$this->plugin_url = str_replace( basename( $this->plugin_file ), '', plugins_url( basename( $this->plugin_file ), $this->plugin_file ) );
+
+			do_action( 'ajax_plugin_loaded');
 		}
 
 		/**
 		 * Enable hooks for the plugin
 		 */
 		public function enable_hooks() {
-			add_action( 'ajax_plugin_loaded', [$this, 'load_assets'] );
- //        add_action( 'wp_enqueue_scripts', [ $this, 'add_jquery' ] );
-//			add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_ajax_button_script' ] );
+//			add_action( 'ajax_plugin_loaded', [$this, 'load_assets'] );
+            add_action( 'wp_enqueue_scripts', [ $this, 'add_jquery' ] );
+			add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_ajax_button_script' ] );
 			add_shortcode( 'ajax_button', [ $this, 'ajax_button_shortcode' ] );
 		}
 
@@ -62,83 +73,82 @@ if ( ! class_exists( 'Dokus__Ajax__Inspector' ) ) {
 		 * Disable hooks for the plugin
 		 */
 		public function disable_hooks() {
-//			remove_action( 'wp_enqueue_scripts', [ $this, 'add_jquery' ] );
-//			remove_action( 'wp_enqueue_scripts', [ $this, 'enqueue_ajax_button_script' ] );
+			remove_action( 'wp_enqueue_scripts', [ $this, 'add_jquery' ] );
+			remove_action( 'wp_enqueue_scripts', [ $this, 'enqueue_ajax_button_script' ] );
 			remove_shortcode( 'ajax_button' );
 		}
 
 
-//		/**
-//		 * Enqueues jquery
-//		 *
-//		 * @return void
-//		 */
-//		public function add_jquery(): void {
-//			wp_enqueue_script( 'jquery' );
-//		}
-//
-//		/**
-//		 * Enqueues the custom JS script for the button
-//		 *
-//		 * @return void
-//		 */
-//		public function enqueue_ajax_button_script(): void {
-//			// Enqueue the CSS file.
-//			wp_enqueue_style(
-//				'style',
-//				plugin_dir_url( __FILE__ ) . '../resources/css/style.css',
-//				[],
-//				'1.0'
-//			);
-//
-//            // Enqueue the JS script.
-//            wp_enqueue_script(
-//				'ajax-button-script',
-//				plugin_dir_url( __FILE__ ) . '../resources/js/ajax-button-script.js',
-//				[ 'jquery' ],
-//				'1.0',
-//				true
-//			);
-//			// Localize script with nonce to MyAjax object
-//			wp_localize_script('ajax-button-script', 'ajax_button_script_vars', array(
-//				'ajaxurl' => admin_url('admin-ajax.php'), // This line localizes the 'ajaxurl' variable
-//			));
-//		}
-
-    public function load_assets (){
-	    // These ones will be enqueued on `admin_enqueue_scripts` if the conditional method on filter is met
-	    // NOTE TO SELF: need to track down this conditional (also need to think about what conditional do I want to load my plugin with?)
-	    tribe_assets(
-		    $this,
-		    [
-			    [ 'ajax-button-script', plugin_dir_url( __FILE__ ) . '../resources/js/ajax-button-script.js', [ 'jquery' ] ],
-			    [ 'ajax-style', plugin_dir_url( __FILE__ ) . '../resources/css/style.css' ],
-		    ],
-		    'admin_enqueue_scripts',
-		    [
-			    'conditionals' => [ $this, 'should_enqueue_assets' ],
-			    'localize'     => (object) [
-				    'name' => 'ajax-button-script',
-				    'data' => [
-					    'check' => wp_create_nonce( 'ajax-nonce' ),
-                        'ajax-url'  => admin_url('admin-ajax.php')
-				    ]
-                    ]
-                ]
-
-	    );
-    }
+		/**
+		 * Enqueues jquery
+		 *
+		 * @return void
+		 */
+		public function add_jquery(): void {
+			wp_enqueue_script( 'jquery' );
+		}
 
 		/**
-		 * Checks whether the assets should be enqueued.
+		 * Enqueues the custom JS script for the button
 		 *
-		 * @since 4.15.0
-		 *
-		 * @return boolean True if the assets should be enqueued.
+		 * @return void
 		 */
-		public function should_enqueue_assets() {
-			return true;
+		public function enqueue_ajax_button_script(): void {
+			// Enqueue the CSS file.
+			wp_enqueue_style(
+				'style',
+				plugin_dir_url( __FILE__ ) . '../resources/css/style.css',
+				[],
+				'1.0'
+			);
+
+            // Enqueue the JS script.
+            wp_enqueue_script(
+				'ajax-button-script',
+				plugin_dir_url( __FILE__ ) . '../resources/js/ajax-button-script.js',
+				[ 'jquery' ],
+				'1.0',
+				true
+			);
+			// Localize script with nonce to MyAjax object
+			wp_localize_script('ajax-button-script', 'ajax_button_script_vars', array(
+				'ajaxurl' => admin_url('admin-ajax.php'), // This line localizes the 'ajaxurl' variable
+			));
 		}
+
+//    public function load_assets (){
+//	    tribe_assets(
+//		    $this,
+//		    [
+//			    [ 'ajax-button-script', plugin_dir_url( __FILE__ ) . '../resources/js/ajax-button-script.js', [ 'jquery' ] ],
+//			    [ 'ajax-style', plugin_dir_url( __FILE__ ) . '../resources/css/style.css' ],
+//		    ],
+//		    'admin_enqueue_scripts',
+//		    [
+//			    'conditionals' => [ $this, 'should_enqueue_assets' ],
+//			    'localize'     => (object) [
+//				    'name' => 'ajax-button-script',
+//				    'data' => [
+//					    'check' => wp_create_nonce( 'ajax-nonce' ),
+//                        'ajax-url'  => admin_url('admin-ajax.php')
+//				    ]
+//                    ]
+//                ]
+//
+//	    );
+//    }
+//
+//		/**
+//		 * Checks whether the assets should be enqueued.
+//		 *
+//		 * @since 4.15.0
+//		 *
+//		 * @return boolean True if the assets should be enqueued.
+//		 */
+//		public function should_enqueue_assets() {
+//			// Eventually need to make this to check if the shortcode or widget is on the current page
+//            return true;
+//		}
 
 		/**
 		 * Creates the shortcode that outputs the button
