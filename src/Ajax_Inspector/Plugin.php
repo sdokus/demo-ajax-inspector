ef80eb43416dd1f636efd486aa2059af4aa2f13b
Sami Dokus <sami2464@gmail.com>
2024-02-23 14:22:29 -0700

    Stuck with how to get tribe_asset() to work

--- src/Ajax_Inspector/Plugin.php
+++ src/Ajax_Inspector/Plugin.php
@@ -90,7 +90,7 @@ class Plugin {
 	 */
 	public function boot( string $file ): void {
 		// Sets up the hooks for the plugin.
-        $this->enable_hooks();
+		$this->enable_hooks();
 
 		// Set up the plugin provider properties.
 		$this->plugin_file = $file;
@@ -98,7 +98,7 @@ class Plugin {
 		$this->plugin_dir  = trailingslashit( basename( $this->plugin_path ) );
 		$this->plugin_url  = str_replace( basename( $this->plugin_file ), '', plugins_url( basename( $this->plugin_file ), $this->plugin_file ) );
 
-        // Now that the plugin is set up, enqueue the assets.
+		// Now that the plugin is set up, enqueue the assets.
 		do_action( 'sdokus_ajax_plugin_loaded' );
 	}
 
@@ -108,8 +108,8 @@ class Plugin {
 	 * @since 1.0.0
 	 */
 	public function enable_hooks() {
-	//	add_action( 'sdokus_ajax_plugin_loaded', [$this, 'load_assets'] );
-		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_ajax_button_script' ] );
+		add_action( 'sdokus_ajax_plugin_loaded', [ $this, 'load_assets' ] );
+		//	add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_ajax_button_script' ] );
 		add_action( 'wp_ajax_sdokus_get_events_list', [ $this, 'get_events_callback' ] );
 		add_shortcode( 'ajax_button', [ $this, 'ajax_button_shortcode' ] );
 	}
@@ -120,46 +120,54 @@ class Plugin {
 	 * @since 1.0.0
 	 */
 	public function disable_hooks() {
-		remove_action( 'sdokus_ajax_plugin_loaded', [$this, 'load_assets'] );
+		remove_action( 'sdokus_ajax_plugin_loaded', [ $this, 'load_assets' ] );
 		remove_action( 'wp_enqueue_scripts', [ $this, 'enqueue_ajax_button_script' ] );
 		remove_action( 'wp_ajax_sdokus_get_events_list', [ $this, 'get_events_callback' ] );
 		remove_shortcode( 'ajax_button' );
 	}
 
-	/**
-	 * Enqueues the custom JS script for the button.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @return void
-	 */
-	public function enqueue_ajax_button_script(): void {
-		// Enqueue the CSS file.
-		wp_enqueue_style(
-			'sdokus-ajax-inspector-style',
-			$this->plugin_url . 'src/resources/css/style.css',
+	public function load_assets() {
+		tribe_asset(
+			$this,
+            'sdokus-ajax-inspector-style',
+			'style.css',
 			[],
-			'1.0'
+			'admin_enqueue_scripts',
 		);
 
-		// Enqueue the JS script.
-		wp_enqueue_script(
-			'sdokus-ajax-inspector-buttons',
-			$this->plugin_url . 'src/resources/js/ajax-button-script.js',
-			[ 'jquery', 'wp-i18n' ],
-			'1.0',
-			true
-		);
+        tribe_asset(
+          $this, // $origin
+          'sdokus-ajax-inspector-buttons', // $slug
+          'ajax-button-script.js', // $file
+          ['jquery', 'wp-i18n'], // $deps
+          'admin_enqueue_scripts', // $action
+          [
+              'localize' => [
+	          [
+		          'name' => 'ajax_button_script_vars',
+		          'data' => [
+			          'ajaxurl'  => admin_url( 'admin-ajax.php' ),
+			          //'rest_url' => get_rest_url( null, '/tribe/events/v1/events/?per_page=10' ), // (throwing a fatal currently)
+			          'event_happening_label' => esc_html__( '%1$s happening on %2$s', 'sdokus-ajax-inspector' )
 
-		// Localize script with nonce to MyAjax object
-		wp_localize_script( 'sdokus-ajax-inspector-buttons', 'ajax_button_script_vars', [
-			'ajaxurl'  => admin_url( 'admin-ajax.php' ),
-			'rest_url' => get_rest_url( null, '/tribe/events/v1/events/?per_page=10' ),
-			'event_happening_label' => esc_html__( '%1$s happening on %2$s', 'sdokus-ajax-inspector' )
-		] );
+		          ],
+	          ]]
+          ] // $arguments
+        );
+	}
 
-		// Set up translations for the script
-		wp_set_script_translations( 'sdokus-ajax-inspector-buttons', 'sdokus-ajax-inspector' );
+
+	/**
+	 * Checks whether the assets should be enqueued.
+	 *
+	 * @since 4.15.0
+	 *
+	 * @return boolean True if the assets should be enqueued.
+	 */
+	public
+	function should_enqueue_assets() {
+		// Eventually need to make this to check if the shortcode or widget is on the current page
+		return true;
 	}
 
 	/**
