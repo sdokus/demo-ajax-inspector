075b5e7193ccde92b4fb67d13b5842ee2458d28d
Sami Dokus <sami2464@gmail.com>
2024-03-20 16:43:15 -0600

    Modularized plugin into Admin pages and Shortcode

--- src/Ajax_Inspector/Plugin.php
+++ src/Ajax_Inspector/Plugin.php
@@ -17,6 +17,12 @@ namespace Sdokus\Ajax_Inspector;
 class Plugin {
 
 	/**
+     * The current version of the plugin.
+     *
+	 * @since 1.0.0
+	 */
+    public const VERSION = '1.0.0';
+	/**
 	 * @since 1.0.0
 	 *
 	 * @var string Plugin File.
@@ -58,11 +64,11 @@ class Plugin {
 	 *
 	 * @since 1.0.0
 	 *
-	 * @return self
+	 * @return static
 	 */
-	public static function get_instance(): self {
+	public static function get_instance() {
 		if ( ! isset( self::$instance ) ) {
-			self::$instance = new static();
+			self::$instance = new self();
 		}
 
 		return self::$instance;
@@ -74,7 +80,7 @@ class Plugin {
 	 * @since 1.0.0
 	 */
 	protected function __construct() {
-		// Intentionally empty.
+        // Intentionally left empty.
 	}
 
 	/**
@@ -106,161 +112,23 @@ class Plugin {
 	 * @since 1.0.0
 	 */
 	public function enable_hooks() {
-		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_ajax_button_script' ] );
-		add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_ajax_button_script' ] );
 		add_action( 'wp_ajax_sdokus_get_events_list', [ $this, 'get_events_callback' ] );
-		add_action( 'admin_menu', [ $this, 'add_shortcode_menu_page' ] );
 		add_action( 'admin_notices', [ $this, 'ajax_demo_notice' ] );
-		add_shortcode( 'ajax_button', [ $this, 'ajax_button_shortcode' ] );
+
+
+        add_action('sdokus_ajax_plugin_loaded', [$this , 'load'], 15);
 	}
 
-	public function enqueue_ajax_button_script(): void {
-		// Enqueue the appropriate CSS file depending if on the frontend or backend
-		if ( ! is_admin() ) {
-			wp_enqueue_style(
-				'sdokus-ajax-inspector-style',
-				$this->plugin_url . 'src/resources/css/style.css',
-				[],
-				'1.0'
-			);
-		} else {
-			wp_enqueue_style(
-				'sdokus-ajax-inspector-admin-style',
-				$this->plugin_url . 'src/resources/css/ajax-admin.css',
-				[],
-				'1.0'
-			);
-		}
-
-		// Enqueue the JS script.
-		wp_enqueue_script(
-			'sdokus-ajax-inspector-buttons',
-			$this->plugin_url . 'src/resources/js/ajax-button-script.js',
-			[ 'jquery', 'wp-i18n' ],
-			'1.0',
-			true
-		);
-
-		// Localize script with nonce to MyAjax object
-		wp_localize_script(
-			'sdokus-ajax-inspector-buttons',
-			'ajax_button_script_vars',
-			[
-				'ajaxurl'               => admin_url( 'admin-ajax.php' ),
-				'rest_endpoint'         => [
-					'base'   => get_rest_url(),
-					'events' => tribe_events_rest_url( '/events' ),
-					'tags'   => get_rest_url( null, '/wp/v2/tags' ),
-				],
-				'nonce'                 => wp_create_nonce( 'wp_rest' ),
-				'event_happening_label' => esc_html__( '%1$s happening on %2$s', 'sdokus-ajax-inspector' ),
-			]
-		);
-
-		// Set up translations for the script
-		wp_set_script_translations( 'sdokus-ajax-inspector-buttons', 'sdokus-ajax-inspector' );
-	}
-
-	/**
-	 * Adds a menu page to the WordPress dashboard.
-	 *
-	 * @since 1.0.0
-	 */
-	public function add_shortcode_menu_page() {
-		add_menu_page(
-			'AJAX Demo',
-			'AJAX Demo',
-			'administrator',
-			'ajax-demo',
-			[ $this, 'render' ],
-		);
-
-		add_submenu_page(
-			'ajax-demo',
-			'AJAX Logger',
-			'AJAX Logger',
-			'administrator',
-			'ajax-demo-2',
-			[ $this, 'ajax_logger' ],
-		);
-	}
-
-	/**
-	 * Callback function to render the shortcode demo content on an admin menu page.
-	 *
-	 * @since 1.0.0
-	 */
-	public function render() {
-		?>
-        <div class="sdokus-ajax-demo-settings">
-            <h1><?php esc_html_e( 'AJAX Demo', 'sdokus-ajax-inspector' ); ?></h1>
-            <div class="sdokus-ajax-demo-settings-header">
-                <h2><?php esc_html_e( 'Grab Events Using AJAX!', 'sdokus-ajax-inspector' ); ?></h2>
-                <p><?php
-					esc_html_e( 'Use the form below to create an AJAX call to return events using either the TEC ORM or TEC REST API', 'sdokus-ajax-inspector' )
-					?>
-                </p>
-            </div>
-
-			<?php echo do_shortcode( '[ajax_button]' ); ?>
-        </div>
-		<?php
-	}
+    public function load():void{
+        Admin\Inspector_Page::get_instance();
+        Admin\Logger_Page::get_instance();
+        Shortcode\Ajax_Button::get_instance();
+    }
 
 	public function ajax_demo_notice() {
 		?>
         <div class="notice notice-success is-dismissible" id="sdokus-ajax-notice">
             <div id="sdokus-ajax-message-container"></div>
-        </div>
-		<?php
-	}
-
-	/**
-	 * @return void
-	 */
-	public function ajax_logger() {
-		$pp_time   = microtime( true );
-		$last_time = microtime( true );
-		$uuid      = uniqid();
-
-		add_action( 'all', function () use ( $pp_time, $uuid, $last_time ) {
-			if ( ! is_admin() ) {
-				return;
-			}
-
-			$ct        = microtime( true );
-			$delta     = $ct - $last_time;
-			$last_time = microtime( true );
-
-			if ( $delta > 0.002 ) {
-				// Get the current filter
-				$current_filter = current_filter();
-
-				// Format the output with filter and time
-				$output = sprintf( '<strong>%s:</strong> %s seconds', $current_filter, number_format( $delta, 5 ) );
-
-				// Enqueue the script
-				wp_enqueue_script(
-					'ajax-logger-script',
-					$this->plugin_url . 'src/resources/js/ajax-log-script.js', [ 'jquery' ],
-					'1.0',
-					true
-				);
-
-				// Use JavaScript to append the content to the sdokus-ajax-log-container when the button is clicked
-				wp_add_inline_script( 'ajax-logger-script', "
-                jQuery(document).ready(function($) {
-                    $('#sdokus-ajax-log-container').append('$output<br>');
-                });
-            " );
-			}
-		} );
-		// Output HTML for your submenu along with the captured content and the button
-		?>
-        <div>
-            <h1><?php esc_html_e( 'Filter Log', 'sdokus-ajax-inspector' ); ?></h1>
-            <label for="sdokus-ajax-log-container"><?php esc_html_e( 'Output:', 'sdokus-demo-ajax-inspector' ); ?></label>
-            <div id="sdokus-ajax-log-container"></div>
         </div>
 		<?php
 	}
@@ -285,68 +153,5 @@ class Plugin {
 		wp_send_json( [ 'events' => $events ] );
 
 		wp_die();
-	}
-
-	/**
-	 * Creates the shortcode that outputs the button.
-	 *
-	 * @since 1.0.0
-	 */
-	public function ajax_button_shortcode() {
-		ob_start();
-		?>
-        <div class="sdokus-ajax-inspector">
-            <h3><?php esc_html_e( 'AJAX Demo', 'sdokus-demo-ajax-inspector' ); ?></h3>
-
-            <!-- Form for Parameters -->
-            <form class="sdokus-ajax-parameters">
-                <fieldset>
-                    <legend><?php esc_html_e( 'Event Parameters', 'sdokus-demo-ajax-inspector' ); ?></legend>
-
-                    <div class="form-group">
-                        <label for="per-page"><?php esc_html_e( 'Per Page: ', 'sdokus-demo-ajax-inspector' ); ?></label>
-                        <input type="number" name="per-page" id="per-page" value="10">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="starts-after"><?php esc_html_e( 'Starts After: ', 'sdokus-demo-ajax-inspector' ); ?></label>
-                        <input type="date" name="starts-after" id="starts-after">
-                    </div>
-                </fieldset>
-            </form>
-
-            <!-- DropDown for which method to use in AJAX call-->
-            <div class="sdokus-ajax-request-method">
-                <label for="sdokus-ajax-request-method"><?php esc_html_e( 'How Do You Want to Get Events?', 'sdokus-demo-ajax-inspector' ); ?></label>
-                <select name="sdokus-ajax-request-method" id="sdokus-ajax-request-method">
-                    <option value="orm"><?php esc_html_e( 'ORM', 'sdokus-demo-ajax-inspector' ); ?></option>
-                    <option value="api"><?php esc_html_e( 'API', 'sdokus-demo-ajax-inspector' ); ?></option>
-                </select>
-            </div>
-
-            <!-- Button for ORM Calls -->
-            <div id="orm-button" class="ajax-button">
-                <div class="sdokus-ajax-buttons">
-					<?php submit_button( 'Get Events Using TEC ORM', 'primary', 'sdokus-ajax-orm-button' ); ?>
-                </div>
-            </div>
-
-            <!-- Button for API Calls -->
-            <div id="api-button" class="ajax-button" style="display: none;">
-                <div class="sdokus-ajax-buttons">
-					<?php submit_button( 'Get Events Using TEC REST API', 'primary', 'sdokus-ajax-api-button' ); ?>
-                </div>
-            </div>
-
-            <!-- If the shortcode is on the frontend, we need the output to be directly on the page instead of a WP notice -->
-			<?php
-			if ( ! is_admin() ) {
-				echo '<div id="sdokus-ajax-notice"><div id="sdokus-ajax-message-container"></div></div>';
-			}
-			?>
-
-        </div>
-		<?php
-		return ob_get_clean();
 	}
 }
